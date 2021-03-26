384661911c23b542f2e61b5d21d26586b6b5dcca
XANDRA <tuhudohoi102@gmail.com>
2021-03-25 20:38:42 -0700

    change firebase API key

---
+++ scripts/firebase_api.js
@@ -0,0 +1,19 @@
+//---------------------------------------------------
+// replace the lines below with your own "firebaseConfig"
+// key value pairs
+//---------------------------------------------------
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD2MlqlqIaVOwyPkES9xgyufBeAl4M6eOQ",
+    authDomain: "team-nxp.firebaseapp.com",
+    projectId: "team-nxp",
+    storageBucket: "team-nxp.appspot.com",
+    messagingSenderId: "686610384488",
+    appId: "1:686610384488:web:6310d9c5333e9f25badddd",
+    measurementId: "G-KB8MM2HMGE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
