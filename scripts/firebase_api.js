e5e30cf29dbf645e93741ba6aedb6c5a7a4d8f0b
XANDRA <tuhudohoi102@gmail.com>
2021-03-26 14:11:03 -0700

    worked code for getting user input is in the firebase_api.js, work only with testing.html

--- scripts/firebase_api.js
+++ scripts/firebase_api.js
@@ -7,6 +7,7 @@
 var firebaseConfig = {
     apiKey: "AIzaSyD2MlqlqIaVOwyPkES9xgyufBeAl4M6eOQ",
     authDomain: "team-nxp.firebaseapp.com",
+    databaseURL: "https://team-nxp-default-rtdb.firebaseio.com",
     projectId: "team-nxp",
     storageBucket: "team-nxp.appspot.com",
     messagingSenderId: "686610384488",
@@ -17,3 +18,74 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+////////////////////////////
+////////////////////////////////////////////////
+
+    function getCity() { document.getElementById("submit").addEventListener('click', function () {
+        firebase.auth().onAuthStateChanged(function (user) {
+    var location = document.getElementById("city").value;
+    var loc =  document.getElementById("city1").value;
+    var loc2 = document.getElementById("city2").value;
+    console.log(location);
+    console.log(loc);
+    console.log(loc2);
+
+        db.collection("reviews")
+            .add({
+            "name": loc,   //from text field
+            "name2": loc2,
+            "name3": location
+        })
+
+})
+                       })
+                       }
+
+getCity();
+
+function getCity2() { document.getElementById("submit2").addEventListener('click', function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        var location = document.getElementById("name").value;
+        var loc =  document.getElementById("devname").value;
+        var loc2 = document.getElementById("desc").value;
+        console.log(location);
+        console.log(loc);
+        console.log(loc2);
+
+        db.collection("reviews")
+            .add({
+            "name": loc,   //from text field
+            "name2": loc2,
+            "name3": location
+        })
+
+    })
+})
+                   }
+
+getCity2();
+
+
+// Get a reference to the database service
+$(document).ready(function() {
+
+    function writeCities() {
+        var citiesRef = db.collection("apps");
+        citiesRef.add({
+            code: "new"
+        });
+    }
+    writeCities();
+});
+
+function addTemplate() {
+            db.collection("template")
+                .doc("description")
+                .get()
+                .then(function(doc){
+                var n = doc.data().template;
+                $("#desc").text(n);
+            })
+        }
+addTemplate();
