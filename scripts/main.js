d367ae7fc34710d5277c4202f716641c4714809f
Laurent Nguyen <laurent2631@yahoo.com>
2021-04-15 07:23:27 -0700

    Laurents main.js changes

--- scripts/main.js
+++ scripts/main.js
@@ -34,25 +34,6 @@ $(document).ready(function() {
                     var name = doc.data().name;
                     var dev_name = doc.data().dev_name;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    var codeString = '<div class="card mb-3" style = "width: 90%>' +
-                        '<div class="row g-0 rows-cols-auto">' +
-                        '<div class="col-4">' +
-                        '<img src="images/pineapple.jpg" class="card-img-top" style = "width: 100px; padding: 5pt";>' +
-                        '</div>' +
-                        '<div class="col-7 align self-center">' +
-                        '<div>' +
-                        '<h4>' + name + '</h4>' +
-                        '<p>By ' + dev_name + '</p>' +
-                        '</div>' + '</div>'
-=======
-                    var codeString = '<div class="card-body">' +
-                        '<h2 class="card-title">' + name + '</h2>' +
-                        '<p class="card-text">By' + dev_name + '</p>' +
-                        '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>'
->>>>>>> 6f5a64336e2a3f7455ae4713cd96f946f43abe72
-=======
                     var codeString = '<div class="mb-3" style = "width: 90%">' +
                         '<div class="row g-0 row-cols-auto" >' + '<div class="col-4">' +
                         '<img src="images/pineapple.jpg" style = "width: 100px; padding: 5pt";>' +
@@ -62,7 +43,6 @@ $(document).ready(function() {
                         '<h4>' + name + '</h4>' +
                         '<p>By: ' + dev_name + '</p>' +
                         '</div>' + '</div>'
->>>>>>> fa50dbb50a08037610f85b5eaed8a849f8bb6d18
 
                     $("#app-goes-here").append("<div style='cursor:pointer, color:white' id='" + id + "'>" + codeString + "</div>");
                     addWebcamListener(id);
@@ -82,4 +62,11 @@ $(document).ready(function() {
     }
 
 
+
 });
+
+
+
+
+
+
